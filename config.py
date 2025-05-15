a89c66ba57c833636acdcbd8135257b55f932abd
GANJAKHOR001 <angrysuraj2007@gmail.com>
2025-05-15 19:05:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/GANJAKHOROO1/BrandrdXMusic",
+    "https://github.com/GANJAKHOR001/FINAL_BRAND",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
